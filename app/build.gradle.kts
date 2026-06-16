75c4507042420b40ed278cdee39c5fea12b233bd
seerish17 <seerish17@users.noreply.github.com>
2026-06-16 23:42:50 +0400

    chore: bump version to 1.0.2 for max stealth release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.stalkerhek.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
