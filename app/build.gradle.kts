5361bb5d81251cff76cf50d7f67587ff2ae898b4
seerish17 <seerish17@users.noreply.github.com>
2026-06-17 11:48:05 +0400

    chore: release v1.8.0 with transcendental stealth bypass

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.stalkerhek.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 70
-        versionName = "1.7.0"
+        versionCode = 80
+        versionName = "1.8.0"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
