8754aa55f8db947f7670a4f1c066b34f1405de7a
seerish17 <seerish17@users.noreply.github.com>
2026-06-17 14:06:20 +0400

    chore: release v2.0.1 (God Mode Ghost Edition)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.stalkerhek.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 200
-        versionName = "2.0.0"
+        versionCode = 201
+        versionName = "2.0.1"
 
 
         ndk {
