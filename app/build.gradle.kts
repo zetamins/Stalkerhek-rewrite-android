18dcb639ad64073a97b2d3459343809ef90a9ea9
seerish17 <seerish17@users.noreply.github.com>
2026-06-17 10:55:51 +0400

    chore: release v1.6.0 with divine stealth bypass

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.stalkerhek.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 51
-        versionName = "1.5.1"
+        versionCode = 60
+        versionName = "1.6.0"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
