5aa17677b9a4f9d5c893cdd48d6aa3e249cbf389
seerish17 <seerish17@users.noreply.github.com>
2026-06-17 14:51:56 +0400

    chore: release v2.2.0 (The Absolute - Final Edition)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.stalkerhek.tv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 210
-        versionName = "2.1.0"
+        versionCode = 220
+        versionName = "2.2.0"
 
 
         ndk {
